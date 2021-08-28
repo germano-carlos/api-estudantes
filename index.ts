11eef437740e405a7578d6bb66798d2870981267
Carlos Germano <germano.carlos2712@gmail.com>
2021-08-28 11:05:35 -0300

    Commit 01 - Comentário

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const port = process.env.PORT;
 
+// Comentário X (Inicializa a porta para escuta da aplicação)
 app.listen(port, () =>
   console.log(`This beautiful and updated server is running on port ${port}`)
 );
